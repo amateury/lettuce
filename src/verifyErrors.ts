bad719732de9e91c61aaedcd29987b80553912a9
Brayan Andrés Salgado Navarro <binariado@gmail.com>
2022-01-21 00:26:04 -0500

    add documentation

--- src/verifyErrors.ts
+++ src/verifyErrors.ts
@@ -1,14 +1,31 @@
 import {ResponseVerifyErrors, ErrorStatus} from "../functions/verifyErrors";
 
+/**
+ * Generate exception
+ * 
+ * @param response - data response
+ */
 function error(response: ResponseVerifyErrors): ResponseVerifyErrors {
     throw response;
 }
 
+/**
+ * Raise exception for errors handled from a server
+ * 
+ * @param response - data response
+ * @param code - status code
+ */
 function responseServerError(response: ResponseVerifyErrors, code: number): ResponseVerifyErrors {
     const {message, errors} = response;
     throw {"statusCode": code, "message": message, errors};
 }
 
+/**
+ * Throw exception, the response returns the key status code depending on the ecosystem where the deployment is running
+ * 
+ * @param response - data response
+ * @param code - status code
+ */
 export function exception(response: ResponseVerifyErrors, code: number) {
     typeof window !== 'undefined'
     && ({}).toString.call(window) === '[object Window]'
@@ -18,7 +35,7 @@ export function exception(response: ResponseVerifyErrors, code: number) {
 /**
  * validate errors and send message
  *
- * @param errors -
+ * @param errors - Array data errors
  */
 export async function verifyErrors(
     errors: ErrorStatus[]
